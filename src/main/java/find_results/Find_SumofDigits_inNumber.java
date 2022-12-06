aed7747f1ec9357713709108d55063db68cacb07
raslan68 <raslan68@yahoo.com>
2022-12-06 23:59:11 +0100

    Sum plus num

--- src/main/java/find_results/Find_SumofDigits_inNumber.java
+++ src/main/java/find_results/Find_SumofDigits_inNumber.java
@@ -5,12 +5,10 @@ public class Find_SumofDigits_inNumber {
     public static void main(String[] args) {
         int num = 12345;
         int sum = 0;
-        int rev = 0;
 
             while (num > 0) {
 
-                rev = num % 10;
-                sum += rev;
+                sum = sum + num % 10;
                 num = num / 10;
             }
         System.out.println(sum);;
