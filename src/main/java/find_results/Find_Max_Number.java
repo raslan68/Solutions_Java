572e1305b7878095190706fcd67cd8c63009d701
admin <raslan68@yahoo.com>
2022-12-21 23:07:47 +0100

    max number with for loop

--- src/main/java/find_results/Find_Max_Number.java
+++ src/main/java/find_results/Find_Max_Number.java
@@ -1,5 +1,6 @@
 package find_results;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,13 +8,22 @@ public class Find_Max_Number {
 
     public static String find_Max_Number(List<Integer> nums){
         int max = 0;
-        for (Integer num : nums){
-            if (num > max){
-                max=num;
+        for (int i=1; i<nums.size(); i++){
+            if (nums.get(i) > max){
+                max=nums.get(i);
             }
         }
         return "Max number is: " + max;
 
+    }
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(9);
+        list.add(4);
+        list.add(10);
+        list.add(2);
+        System.out.println(find_Max_Number(list));
     }
 }
         //OR
