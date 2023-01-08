57d28188aee5b69bddfc30a94b26d045f37868db
admin <raslan68@yahoo.com>
2023-01-08 13:10:03 +0100

    first commit

---
+++ src/main/java/find_results/Write_Data_In_File.java
@@ -0,0 +1,21 @@
+package find_results;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Write_Data_In_File {
+    public static void main(String[] args) throws IOException {
+
+        FileWriter wr = new FileWriter("C:\\Users\\admin\\Desktop\\javaTestText.txt");
+        BufferedWriter bw = new BufferedWriter(wr);
+
+        bw.write("Hello from Java!\n");
+        bw.write("This message is coming from Java to here!\n");
+        bw.write("Have a nice Day!");
+
+        System.out.println("Done!!!!");
+        bw.close();
+
+    }
+}
