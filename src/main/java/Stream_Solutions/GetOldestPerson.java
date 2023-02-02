009698cd17b720eeaf49f2dce7f7561afd315afd
admin <raslan68@yahoo.com>
2023-02-03 00:02:52 +0100

    new GetOldestPerson Class

---
+++ src/main/java/Stream_Solutions/GetOldestPerson.java
@@ -0,0 +1,23 @@
+package Stream_Solutions;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class GetOldestPerson {
+    public static Person getOldestPerson(List<Person> people){
+        Person oldestPerson = new Person("", 0);
+        for(Person person : people){
+            if (person.getAge() < oldestPerson.getAge()){
+                oldestPerson = person;
+            }
+        }
+        return oldestPerson;
+    }
+    //Stream Solution
+    public static Person getOldestPerson_stream(List<Person> people){
+        return people.stream()
+                .max(Comparator.comparingInt(Person::getAge))
+                .orElse(null);
+    }
+}
