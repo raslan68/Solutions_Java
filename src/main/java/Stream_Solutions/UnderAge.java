a064b01f72d080459e120704d2248af5e9f62a72
admin <raslan68@yahoo.com>
2023-02-05 23:21:34 +0100

    new UnderAge Class

---
+++ src/main/java/Stream_Solutions/UnderAge.java
@@ -0,0 +1,25 @@
+package Stream_Solutions;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UnderAge {
+    public static Set<String> getKidsName(List<Person> people){
+        Set<String> kids = new HashSet<>();
+        for (Person person : people){
+            if (person.getAge() < 18){
+                kids.add(person.getName());
+            }
+        }
+        return kids;
+    }
+    // Stream Solution
+    public static Set<String> getKidsName_stream(List<Person> people){
+        return people.stream()
+                .filter(person -> person.getAge() < 18)
+                .map(person -> person.getName())
+                .collect(Collectors.toSet());
+    }
+}
