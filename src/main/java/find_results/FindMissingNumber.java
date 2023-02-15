b95fc6d0314dbb3581a6874bfbdb1853acf348f4
admin <raslan68@yahoo.com>
2023-02-15 14:05:30 +0100

    FindMissingNumber other solution

--- src/main/java/find_results/FindMissingNumber.java
+++ src/main/java/find_results/FindMissingNumber.java
@@ -1,8 +1,10 @@
 package find_results;
 
+import java.util.HashSet;
+
 public class FindMissingNumber {
 
-    public static int missingNumber(int[] nums){
+      public static int missingNumber(int[] nums){
         int sum = 0;
         int n = nums.length;
 
@@ -11,6 +13,20 @@ public class FindMissingNumber {
         }
         return n*(n+1) /2 - sum;
     }
+
+   /* public static int missingNumber(int[] nums){
+        int n = nums.length;
+        HashSet<Integer> hashSet = new HashSet();
+        for (int i=0; i <n; i++){
+            hashSet.add(nums[i]);
+        }
+        for (int i=0; i<n; i++){
+            if (!hashSet.contains(i)){
+                return i;
+            }
+        }
+        return n;
+    }*/
 
     public static void main(String[] args) {
         int[] missingNum = {0,3,2,5,1};
