9b7dcf2167fec7936d004618018d2e28db0da22b
admin <raslan68@yahoo.com>
2023-02-21 13:25:17 +0100

    New Code added

--- src/main/java/find_results/Reverse_Number_String.java
+++ src/main/java/find_results/Reverse_Number_String.java
@@ -4,16 +4,28 @@ public class Reverse_Number_String {
     public static void main(String[] args) {
 //----------for Integer-------------------------------
         int num = 123;
-        StringBuffer sb = new StringBuffer(String.valueOf(num));
+        int reverse = 0;
+        int lastDigit = 0;
+
+        while (num != 0){
+            lastDigit = num % 10;
+             reverse = reverse * 10 + lastDigit;
+             num /=10;
+        }
+        System.out.println(reverse);
+
+        // OR
+       /* StringBuffer sb = new StringBuffer(String.valueOf(num));
         StringBuffer sbNumber = sb.reverse();
-        System.out.println(sbNumber);
+        System.out.println(sbNumber);*/
 
 //---------------for String-----------------------------
         String str = "java";
         String rev="";
 
         for (int i=str.length()-1; i>=0; i--){
-            rev= rev + str.charAt(i);
+            rev += str.charAt(i);
+            str.toString();
             //rev= rev + str.toCharArray()[i];  -> also possible
         }
         System.out.println(rev);
