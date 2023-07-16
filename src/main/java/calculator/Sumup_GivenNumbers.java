465faee424c08886e1f5b2cc7ad9fff15ca081fa
admin <raslan68@yahoo.com>
2023-07-16 17:39:58 +0200

    Update Sumup_GivenNumbers

--- src/main/java/calculator/Sumup_GivenNumbers.java
+++ src/main/java/calculator/Sumup_GivenNumbers.java
@@ -5,14 +5,31 @@ import java.util.Scanner;
 public class Sumup_GivenNumbers {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int num;
-        int sum = 0;
-        System.out.println("Enter your number: ");
 
-        while ((num = scan.nextInt()) != 0) {
-            sum += num;
-            System.out.println("Total is :" + sum);
+        int num ;
+        int sum = 0;
+        int attempt = 0;
+
+        while(attempt <= 3) {
+            System.out.println("Enter your number: ");
+            Scanner scan = new Scanner(System.in);
+            while (!scan.hasNextInt()){
+                System.out.println("Enter only Number please!");
+                scan.nextLine();
+            }
+            num = scan.nextInt();
+            if (num !=0) {
+                    sum += num;
+                    System.out.println("sum: " + sum);
+                attempt++;
+                }
+            }
+        System.out.println("TOTAL is: "+ sum);
+        if (sum >= 20){
+            System.out.println("Congratulations, You win!");
+        }else {
+            System.out.println("Sorry, You lost.");
         }
     }
+
 }
